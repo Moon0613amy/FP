d3c02f8709ed02e3462cacf67395be063cd5feb1
gabriela <gabriela.poncelucas@gmail.com>
2023-08-19 08:51:36 -0500

    1.py

--- condicionales/1.py
+++ condicionales/1.py
@@ -18,6 +18,7 @@ if unidades  >= 51 : precio = 23
 #elif unidades >= 51 : precio = 23
 
 precio = 27 if unidades <= 25 else 23 if unidades >= 51 else 25
+
 compra = unidades * precio
 
 #descuento = 0.05 * compra
@@ -25,5 +26,7 @@ compra = unidades * precio
 
 descuento = ( 0.15 if unidades >= 50 else 0.05 ) * compra
 
-print( f"Precio   :S/ {precio:.2f}" )
-print( f"Compra   :S/ {compra:.2f}" )
+print( f"Precio    :S/ {precio:.2f}" )
+print( f"Compra    :S/ {compra:.2f}" )
+print( f"Descuento :S/ {descuento:.2f}" )
+print( f"Total     :S/ {compra-descuento:.2f}" )
