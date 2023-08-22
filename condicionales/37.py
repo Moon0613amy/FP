d21bcaeb6d9445b1c727a03314df88942a49d0b8
gabriela <gabriela.poncelucas@gmail.com>
2023-08-21 19:48:32 -0500

    37.py

--- condicionales/37.py
+++ condicionales/37.py
@@ -1,5 +1,5 @@
-
-
+import os
+os.system("cls") 
 
 
 Pamela = int(input( "Pamela : ") )
@@ -11,6 +11,21 @@ mitad = ( Pamela + Karol + Fany ) // 2
 mensaje = ""
 if Pamela > mitad  : mensaje ="Gano Pamela"
 elif Karol > mitad : mensaje = "Gano Karol"
-elif Fany > mitad : mensaje = "Gano Fany"
+elif Fany > mitad : mensaje  = "Gano Fany"
 
-elif Pamela < Karol and Pamela < Fany : mensaje 
+elif Pamela < Karol and Pamela < Fany : mensaje = "Pasan a 2da vuelta : Karol y Fany"
+elif Karol < Pamela and Karol< Fany : mensaje = "Pasan a 2da vuelta : Pamela y Fany"
+elif Fany < Pamela and Fany < Karol : mensaje = "Pasan a 2da vuelta : Pamela y Karol"
+
+elif Pamela == Karol and Pamela == Fany and Karol == Fany : mensaje = "Elecciones anuladas"
+elif Pamela > Karol and Pamela > Fany and Karol == Fany : mensaje = "Elecciones anuladas"
+elif Karol > Pamela and Karol > Fany and Pamela == Fany : mensaje = "Elecciones anuladas"
+elif Fany > Pamela and Fany > Karol and Pamela== Karol : mensaje = "Elecciones anuladas"
+
+lista = { "Pamela : ": Pamela, "Karol : ": Karol, "Fany : ": Fany }
+orden = sorted(lista.items(), key=operator.itemgetter(1),  reverse=True)
+
+print(mensaje)
+print ( f"1er Lugar : { orden[0] }" )
+print ( f"2do Lugar : { orden[1] }" )
+print ( f"3er Lugar : { orden[2] }" )
